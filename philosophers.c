1f49410a3a02067f949d6cf5a9a853e7872396b4
Tiago Vargas <tiago.ce10@outlook.com>
2023-03-31 09:44:36 -0300

    refactor to use sushis and hashis

--- philosophers.c
+++ philosophers.c
@@ -7,16 +7,16 @@
 #include <assert.h>
 
 #define N_PHILOSOPHERS 5
-#define N_FORKS 5
+#define N_hashiS 5
 #define DELAY 5000
-#define COOKIES_ON_PLATE 10
+#define SUSHIS_ON_PLATE 10
 
 void *philosopher(void *id);
-void grab_fork(int, int, char *);
-void down_forks(int, int);
+void grab_hashi(int, int, char *);
+void down_hashis(int, int);
 int food_on_table(int philosopher_id);
 
-pthread_mutex_t forks[N_FORKS];
+pthread_mutex_t hashis[N_hashiS];
 pthread_t philosophers[N_PHILOSOPHERS];
 pthread_mutex_t food_lock;
 int sleep_seconds = 0;
@@ -33,8 +33,8 @@ int main(int argc, char **argv)
 	pthread_mutex_init(&food_lock, NULL);
 
 	// Inicializa os garfos
-	for (i = 0; i < N_FORKS; i++)
-		pthread_mutex_init(&forks[i], NULL);
+	for (i = 0; i < N_hashiS; i++)
+		pthread_mutex_init(&hashis[i], NULL);
 
 	// Inicializa os filósofos
 	for (i = 0; i < N_PHILOSOPHERS; i++)
@@ -50,24 +50,24 @@ int main(int argc, char **argv)
 void *philosopher(void *num)
 {
 	int id;
-	int left_fork, right_fork;
+	int left_hashi, right_hashi;
 
 	id = (int) num;
 	printf ("Philosopher %d: is done thinking and now ready to eat.\n", id);
-	right_fork = id;
-	left_fork = (id + 1) % N_PHILOSOPHERS;
+	right_hashi = id;
+	left_hashi = (id + 1) % N_PHILOSOPHERS;
 
-	int remaining_cookies;
-	while (remaining_cookies = food_on_table(id))
+	int remaining_sushis;
+	while (remaining_sushis = food_on_table(id))
 	{
-		grab_fork(id, right_fork, "right");
-		grab_fork(id, left_fork, "left");
+		grab_hashi(id, right_hashi, "right");
+		grab_hashi(id, left_hashi, "left");
 
 		pthread_mutex_unlock(&food_lock);
 
 		printf("Philosopher %d: eating.\n", id);
-		usleep (DELAY * (COOKIES_ON_PLATE - remaining_cookies + 1));
-		down_forks (left_fork, right_fork);
+		usleep (DELAY * (SUSHIS_ON_PLATE - remaining_sushis + 1));
+		down_hashis (left_hashi, right_hashi);
 	}
 
 	printf ("Philosopher %d: is done eating.\n", id);
@@ -75,35 +75,35 @@ void *philosopher(void *num)
 
 int food_on_table(int philosopher_id)
 {
-	static int remaining_cookies = COOKIES_ON_PLATE;
+	static int remaining_sushis = SUSHIS_ON_PLATE;
 
 	pthread_mutex_lock(&food_lock);
 
 	// Tá comendo
-	if (remaining_cookies > 0){
-		remaining_cookies--;
-		printf("Philosopher %d: get your food, %d remaining cookies.\n", philosopher_id, remaining_cookies);
+	if (remaining_sushis > 0){
+		remaining_sushis--;
+		printf("Philosopher %d: get your food, %d remaining sushis.\n", philosopher_id, remaining_sushis);
 	}
 
 	//Caso os biscoitos acabem eu preciso destravar o food_lock
 	//Do contrário, os filósofos que estavam na fila pra tentar comer jamais entrariam
 	//no while para dar unlock (em philosopher()), assim a execução entraria em deadlock
 	//pois existiria threads que jamais sairiam de sua rotina esperando o unlock
-	if(!remaining_cookies)
+	if(!remaining_sushis)
 		pthread_mutex_unlock(&food_lock);
-	
-	return remaining_cookies;
+
+	return remaining_sushis;
 }
 
-void grab_fork(int philosopher_id, int fork_id, char *side)
+void grab_hashi(int philosopher_id, int hashi_id, char *side)
 {
 	// side = "left" | "right"
-	pthread_mutex_lock(&forks[fork_id]);
-	printf ("Philosopher %d: got %s fork %d\n", philosopher_id, side, fork_id);
+	pthread_mutex_lock(&hashis[hashi_id]);
+	printf ("Philosopher %d: got %s hashi %d\n", philosopher_id, side, hashi_id);
 }
 
-void down_forks(int left_fork, int right_fork)
+void down_hashis(int left_hashi, int right_hashi)
 {
-	pthread_mutex_unlock(&forks[left_fork]);
-	pthread_mutex_unlock(&forks[right_fork]);
+	pthread_mutex_unlock(&hashis[left_hashi]);
+	pthread_mutex_unlock(&hashis[right_hashi]);
 }
